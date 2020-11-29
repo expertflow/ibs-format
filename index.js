d846fc7ff524bae7d91442410ff09bcfc6b2af4f
Raza ellahi <48081394+razaellahi@users.noreply.github.com>
2020-11-29 18:05:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,282 +1,288 @@
-function ibsFormat(value, arr, linky) {
-    let output = null;
-    if (value != "" && value != null && value != undefined && arr && arr.length > 0) {
-      let counter = 0;
-      let space = "";
-      if (arr[0].constructor === Array) {
-        arr.map(e => {
-          if (counter == 0) {
-            space = " &nbsp";
-          } else {
-            space = "";
-          }
-          e[2] = (e[0].length + 1).toString();
-          if (e[1] == "*") {
-            value = astericHandler(value, e[0], e[1], parseInt(e[2]), space);
-          } else if (e[1] == '**') {
-            value = doubleAstericHandler(value, e[0], e[1], parseInt(e[2]), space);
-          } else {
-            value = getFormat(value, e[0], e[1], parseInt(e[2]), space);
-          }
-          ++counter;
-        });
-      } else {
-        arr[2] = (arr[0].length + 1).toString();
-        if (arr[1] == "*") {
-          value = astericHandler(value, arr[0], arr[1], parseInt(arr[2]), " ");
-        } else if (arr[1] == '**') {
-          value = doubleAstericHandler(value, arr[0], arr[1], parseInt(arr[2]), " ");
+function ibsFormat(value, arr, linky, escaping) {
+  let output = null;
+  escaping = escaping && escaping.allowXssEscaping == false ? false : true;
+  if (value != "" && value != null && value != undefined && arr && arr.length > 0) {
+    if (escaping) {
+      value = value.replace(/</g, "&lt;");
+      value = value.replace(/>/g, "&gt;");
+    }
+    if (arr[0].constructor === Array) {
+      arr.map(function (e) {
+        e[2] = (e[0].length + 1).toString();
+        if (e[1] == "*") {
+          value = astericHandler(value, e[0], e[1], parseInt(e[2]), " ");
+        } else if (e[1] == '**') {
+          value = doubleAstericHandler(value, e[0], e[1], parseInt(e[2]), " ");
         } else {
-          value = getFormat(value, arr[0], arr[1], parseInt(arr[2]), " ");
+          value = getFormat(value, e[0], e[1], parseInt(e[2]), " ");
         }
-      }
-      output = value;
+      });
     } else {
-      output = null;
-    }
-    if (value != "" && value != null && value != undefined && linky && linky.detectLinks == true) {
-      let targ;
-      if (linky.target != null && linky.target != "") {
-        targ = linky.target;
+      arr[2] = (arr[0].length + 1).toString();
+      if (arr[1] == "*") {
+        value = astericHandler(value, arr[0], arr[1], parseInt(arr[2]), " ");
+      } else if (arr[1] == '**') {
+        value = doubleAstericHandler(value, arr[0], arr[1], parseInt(arr[2]), " ");
       } else {
-        targ = "_self";
+        value = getFormat(value, arr[0], arr[1], parseInt(arr[2]), " ");
       }
-      output = linkfy(value, targ);
     }
-    return output;
+    output = value;
+  } else {
+    output = null;
   }
-  
-  function doubleAstericHandler(text, tag, iden, trim, space) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-    let a = text.split(" ");
-    let singleStericCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(/\*\*/g) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(/\*\*/g) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+  if (value != "" && value != null && value != undefined && linky && linky.detectLinks == true) {
+    let targ;
+    if (linky.target != null && linky.target != "") {
+      targ = linky.target;
+    } else {
+      targ = "_self";
+    }
+    output = linkfy(value, targ);
+  }
+  return output ? output.trim() : "";
+}
+
+function doubleAstericHandler(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\*\*/g) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\*\*/g) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(/\*\*/g, "<" + tag + ">");
+          flag = "2";
         } else {
-          if (flag == "1") {
-            e = e.replace(/\*\*/g, "<" + tag + ">");
-            flag = "2";
-          } else {
-            e = e.replace(/\*\*/g, "</" + tag + ">");
-            flag = "1";
-          }
-          ++loopCounter;
+          e = e.replace(/\*\*/g, "</" + tag + ">");
+          flag = "1";
         }
+        ++loopCounter;
       }
-      if (e.length > 1 && (e.match(/\*\*/g) || []).length > 1) {
-        const n = (e.match(/\*\*/g) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf(iden, i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf(iden, v));
-          }
-        }
-        e = e.replace('**', "<" + tag + ">");
-        e = e.replace('**', "</" + tag + ">");
-        box = [];
-      }
-      arr.push(e);
-    });
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + space;
-      } else {
-        finalText = finalText + e + space;
-      }
-    });
-  
-    return finalText;
-  }
-  
-  
-  function astericHandler(text, tag, iden, trim, space) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-    let a = text.split(" ");
-    let singleStericCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(/\x2a/g) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(/\x2a/g) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
-        } else {
-          if (flag == "1") {
-            e = e.replace(/\x2a/g, "<" + tag + ">");
-            flag = "2";
-          } else {
-            e = e.replace(/\x2a/g, "</" + tag + ">");
-            flag = "1";
-          }
-          ++loopCounter;
-        }
-      }
-  
-      if (e.length > 1 && (e.match(/\x2a/g) || []).length > 1) {
-        const n = (e.match(/\x2a/g) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf(iden, i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf(iden, v));
-          }
-        }
-        let firstIndex = box[0];
-        let lastIndex = box[box.length - 1];
-        e = replaceChar(e, "<" + tag + ">", firstIndex);
-        e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
-        box = [];
-      }
-  
-      arr.push(e);
-    });
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + space;
-      } else {
-        finalText = finalText + e + space;
-      }
-    });
-    function replaceChar(origString, replaceChar, index) {
-      let firstPart = origString.substr(0, index);
-      let lastPart = origString.substr(index + 1);
-  
-      let newString = firstPart + replaceChar + lastPart;
-      return newString;
     }
-    return finalText;
-  }
-  
-  
-  function getFormat(text, tag, iden, trim, space) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-    let a = text.split(" ");
-    let singleStericCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(new RegExp(iden, "g")) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes(iden) &&
-        (e.match(new RegExp(iden, "g")) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+    if (e.length > 1 && (e.match(/\*\*/g) || []).length > 1) {
+      const n = (e.match(/\*\*/g) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
         } else {
-          if (flag == "1") {
-            e = e.replace(new RegExp(iden, "g"), "<" + tag + ">");
-            flag = "2";
-          } else {
-            e = e.replace(new RegExp(iden, "g"), "</" + tag + ">");
-            flag = "1";
-          }
-          ++loopCounter;
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
         }
       }
-      if (e.length > 1 && (e.match(new RegExp(iden, "g")) || []).length > 1) {
-        const n = (e.match(new RegExp(iden, "g")) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf(iden, i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf(iden, v));
-          }
-        }
-        let firstIndex = box[0];
-        let lastIndex = box[box.length - 1];
-  
-        e = replaceChar(e, "<" + tag + ">", firstIndex);
-        e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
-  
-        box = [];
-      }
-      arr.push(e);
-    });
-  
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + space;
-      } else {
-        finalText = finalText + e + space;
-      }
-    });
-    function replaceChar(origString, replaceChar, index) {
-      let firstPart = origString.substr(0, index);
-      let lastPart = origString.substr(index + 1);
-  
-      let newString = firstPart + replaceChar + lastPart;
-      return newString;
+      e = e.replace('**', "<" + tag + ">");
+      e = e.replace('**', "</" + tag + ">");
+      box = [];
     }
-    return finalText;
+    arr.push(e);
+  });
+  arr.forEach(function (e) {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+
+  return finalText;
+}
+
+
+function astericHandler(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\x2a/g) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\x2a/g) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(/\x2a/g, "<" + tag + ">");
+          flag = "2";
+        } else {
+          e = e.replace(/\x2a/g, "</" + tag + ">");
+          flag = "1";
+        }
+        ++loopCounter;
+      }
+    }
+
+    if (e.length > 1 && (e.match(/\x2a/g) || []).length > 1) {
+      const n = (e.match(/\x2a/g) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
+        } else {
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
+        }
+      }
+      let firstIndex = box[0];
+      let lastIndex = box[box.length - 1];
+      e = replaceChar(e, "<" + tag + ">", firstIndex);
+      e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
+      box = [];
+    }
+
+    arr.push(e);
+  });
+  arr.forEach(function (e) {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+  function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
   }
-  
-  function linkfy(text, target) {
-    text = text.replace(/&nbsp/g, " ");
-    let expression = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
-    let emailRegex = /(\S+@\S+\.\S+)/gim;
-    let httpVerify = /^((http|https|ftp):\/\/)/;
-    let regex = new RegExp(expression);
-    let a = text.split(" ");
-    let finalText = "";
-    a.map((part, index) => {
-      if (part.match(regex)) {
+  return finalText;
+}
+
+
+function getFormat(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(new RegExp(iden, "g")) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(function (e) {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(new RegExp(iden, "g")) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(new RegExp(iden, "g"), "<" + tag + ">");
+          flag = "2";
+        } else {
+          e = e.replace(new RegExp(iden, "g"), "</" + tag + ">");
+          flag = "1";
+        }
+        ++loopCounter;
+      }
+    }
+    if (e.length > 1 && (e.match(new RegExp(iden, "g")) || []).length > 1) {
+      const n = (e.match(new RegExp(iden, "g")) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
+        } else {
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
+        }
+      }
+      let firstIndex = box[0];
+      let lastIndex = box[box.length - 1];
+
+      e = replaceChar(e, "<" + tag + ">", firstIndex);
+      e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
+
+      box = [];
+    }
+    arr.push(e);
+  });
+
+  arr.forEach(function (e) {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+  function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
+  }
+  return finalText;
+}
+
+function linkfy(text, target) {
+  let strictUrlExpression = /[-a-zA-Z0-9@:%_\+.~#?&\/=]{1,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi;
+  let looseUrlExpression = /^https?\:\/\/[^\/\s]+(\/.*)?$/;
+  let ip4Expression = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/g;
+  let emailRegex = /(\S+@\S+\.\S+)/gim;
+  let httpVerify = /^((http|https|ftp):\/\/)/;
+  let strictUrlRegex = new RegExp(strictUrlExpression);
+  let looseUrlRegex = new RegExp(looseUrlExpression);
+  let ip4Regex = new RegExp(ip4Expression);
+  text = text.replace(/\n/g, " <br> ");
+  let a = text.split(" ");
+  let finalText = "";
+  a.map(function (part, index) {
+    if (part != " " && part != "" && part != undefined && part != "<br>") {
+      if (part.match(looseUrlRegex)) {
+        let ref = part;
+        ref = ref.replace(/<[^>]*>?/gm, "");
+        a[index] = `<a href='${ref}' target='${target}'>${part}</a>`;
+      } else if (part.match(strictUrlRegex)) {
         let ref = part;
         ref = ref.replace(/<[^>]*>?/gm, "");
         if (ref.match(httpVerify)) {
@@ -286,16 +292,22 @@ function ibsFormat(value, arr, linky) {
         } else {
           a[index] = `<a href='http://${ref}' target='${target}'>${part}</a>`;
         }
+      } else if (part.match(ip4Regex)) {
+        let ref = part;
+        ref = ref.replace(/<[^>]*>?/gm, "");
+        a[index] = `<a href='http://${ref}' target='${target}'>${part}</a>`;
       }
-    });
-    a.forEach(e => {
-      if (e == "") {
-        finalText = finalText + "&nbsp";
-      } else {
-        finalText = finalText + e + "&nbsp";
-      }
-    });
-    return finalText;
-  }
-  
-  module.exports.ibsFormat = ibsFormat;
+    }
+  });
+  a.forEach(function (e) {
+    if (e == "") {
+      finalText = finalText + " ";
+    } else {
+      finalText = finalText + e + " ";
+    }
+  });
+  finalText = finalText.replace(/ <br> /g, "<br>");
+  return finalText;
+}
+
+module.exports.ibsFormat = ibsFormat;
